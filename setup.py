e6f2e576f83898ce19c64adb54e87c111b15668e
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-04-02 20:48:31 +0800

    initial

---
+++ setup.py
@@ -0,0 +1,27 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('octo_barnacle/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+REQUIRES = []
+
+setup(
+    name='octo-barnacle',
+    version=version,
+    description='A telegram bot and web api for collecting telegram stickers',
+    author='GYCHEN',
+    author_email='gy.chen@gms.nutc.edu.tw',
+    maintainer='GYCHEN',
+    maintainer_email='gy.chen@gms.nutc.edu.tw',
+    url='https://github.com/gy-chen/octo-barnacle',
+    install_requires=REQUIRES,
+    tests_require=['pytest'],
+    packages=find_packages(),
+)
