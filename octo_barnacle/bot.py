00eac5195532b5589870071a414a96f83aa19b9f
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-04-06 15:33:27 +0800

    add skeletons

---
+++ octo_barnacle/bot.py
@@ -0,0 +1,31 @@
+"""Provide telegram bot related features
+
+Functions:
+  - get_bot()
+  - get_updater()
+
+"""
+
+
+def get_bot():
+    """Get telegram bot object.
+
+    This function will initialize telegram bot with token that received from enviroment.
+
+    Returns:
+      telegram.Bot: python-telegram-bot object 
+    """
+    # TODO
+    pass
+
+
+def get_updater():
+    """Get telegram updater
+
+    This will be entry of starting sticker collect bot.
+
+    Return:
+      telegram.ext.updater.Updater
+    """
+    # TODO
+    pass
---
+++ octo_barnacle/model.py
@@ -0,0 +1,12 @@
+def collect_stickerset(stickerset_id):
+    """Collect specific stickerset
+
+    This function does:
+        - obtain stickerset information from telegram
+        - collect sticker file from telegram
+        - store stickerset data into storage
+
+    This function will acquire a distributed lock, raise AcquireLockException if fail to acquired.
+    """
+    # TODO
+    pass
