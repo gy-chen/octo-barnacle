cc74d8c017059c2a09c1fc4dcfa740e997f2531d
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-04-10 10:28:27 +0800

    autopep8

--- tests/test_model.py
+++ tests/test_model.py
@@ -14,14 +14,15 @@ def test_get_stickerset(bot):
     assert stickerset['name'] == 'Python'
     assert stickerset['title'] == 'Python'
 
+
 def test_get_stickers(bot):
     stickers = list(model._get_stickers(bot, 'python'))
     assert len(stickers) == 1
     sticker = stickers[0]
     assert sticker['stickerset_name'] == 'Python'
     assert sticker['emoji'] == '🐍'
-    assert hashlib.md5(sticker['image']).hexdigest() == '2c5feb400b16bf73c47a68bfd4512181'
-    assert sticker['image_path'].startswith('https://api.telegram.org/file/bot')
+    assert hashlib.md5(sticker['image']).hexdigest(
+    ) == '2c5feb400b16bf73c47a68bfd4512181'
+    assert sticker['image_path'].startswith(
+        'https://api.telegram.org/file/bot')
     assert sticker['image_path'].endswith('stickers/file_0.webp')
-
-    
