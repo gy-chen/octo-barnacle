de48a9bbd194d6485813016bc4df706e98f5586b
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-05-02 21:11:00 +0800

    add delay. make request polite

--- octo_barnacle/data/mal.py
+++ octo_barnacle/data/mal.py
@@ -7,13 +7,24 @@ RecommendationParser
     parse content of https://myanimelist.net/recommendations.php?s=recentrecs&t=anime page
 """
 import itertools
+import time
 import requests
 from bs4 import BeautifulSoup
 
 
 class RecommendationPager:
 
+    USER_AGENT = 'OctoBarnacle/0.1 (+https://github.com/gy-chen/octo-barnacle)'
     BASE_URL = 'https://myanimelist.net/recommendations.php?s=recentrecs&t=anime'
+
+    def __init__(self, delay=None):
+        """init
+
+        Args:
+            delay (int): delay in seconds between every requests
+        """
+        self._delay = delay
+        self._last_req_time = None
 
     def get(self, page):
         """Get content of specific page
@@ -27,12 +38,30 @@ class RecommendationPager:
         Return:
             page content
         """
+        try:
+            self._ensure_delay()
+        except _DelayError:
+            time.sleep(self._delay)
+            return self.get(page)
         show_param = self._convert_to_show_param(page)
         url = "{}&show={}".format(
             self.BASE_URL, self._convert_to_show_param(page))
-        res = requests.get(url)
+
+        headers = {}
+        if self.USER_AGENT:
+            headers['user-agent'] = self.USER_AGENT
+        res = requests.get(url, headers=headers)
         res.raise_for_status()
         return res.text
+
+    def _ensure_delay(self):
+        if self._delay is None:
+            return
+        if self._last_req_time is None:
+            self._last_req_time = time.time()
+            return
+        if self._delay > time.time() - self._last_req_time:
+            raise _DelayError()
 
     def _convert_to_show_param(self, page):
         """show param 100 equal to page 1, 200 equal to page 2, ...etc"""
@@ -45,6 +74,10 @@ class RecommendationPager:
         except requests.HTTPError as e:
             if e.response.status_code != 404:
                 raise e
+
+
+class _DelayError(Exception):
+    pass
 
 
 class RecommendationParser:
