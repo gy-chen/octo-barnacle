e2eedb050a3c4f45834d91bc53692368b0843421
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-05-09 21:15:02 +0800

    add gen_append_range_number

--- octo_barnacle/collectors/utils.py
+++ octo_barnacle/collectors/utils.py
@@ -27,3 +27,24 @@ def gen_remove_whitespace(str_gen):
         generator that generate string
     """
     return (s.translate(_trans_remove_whitespace) for s in str_gen)
+
+
+def gen_append_range_number(str_gen, from_, to):
+    """append number to string from given generator
+
+    beside generate string that appended number, it also generate original string.
+
+    e.g. gen_append_range_number('aaa', 1, 1) -> ('aaa', 'aaa1')
+
+    Arguments:
+        str_gen: expect string iterable
+        from_: start of appendix number range 
+        to: end of appendix number range (include)
+
+    Return:
+        generator that generate string
+    """
+    for s in str_gen:
+        yield s
+        for num in range(from_, to+1):
+            yield '{}{}'.format(s, num)
--- tests/test_collector_utils.py
+++ tests/test_collector_utils.py
@@ -13,3 +13,13 @@ def test_remove_whitespace():
         [' aaaa ', 'bb bb', ' c c c c']))
 
     assert result == ['aaaa', 'bbbb', 'cccc']
+
+
+def test_append_range_number():
+    result = list(utils.gen_append_range_number(
+        ['aaa', 'bbb'],
+        1, 3
+    ))
+
+    assert result == ['aaa', 'aaa1', 'aaa2',
+                      'aaa3', 'bbb', 'bbb1', 'bbb2', 'bbb3']
