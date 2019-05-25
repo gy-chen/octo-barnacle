06dd9552f978f43a69941bd5e7b4e1079e6c8378
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-05-25 17:01:57 +0800

    expanduser

--- octo_barnacle/train/config.py
+++ octo_barnacle/train/config.py
@@ -4,5 +4,6 @@ from ..config import MongoConfig
 
 class ConvertToTfRecordConfig:
     MONGO_CONFIG = MongoConfig
-    TFRECORD_WORKDIR = os.getenv('TF_TFRECORD_WORKDIR', '.')
+    TFRECORD_WORKDIR = os.path.expanduser(
+        os.getenv('TF_TFRECORD_WORKDIR', '.'))
     TFRECORD_FILENAME = os.getenv('TF_TFRECORD_FILENAME', 'stickers.tfrecord')
