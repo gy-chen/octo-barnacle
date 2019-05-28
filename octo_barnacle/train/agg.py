a6c2ba564e778fe7e4d68ffbf1cbfe987dca9e96
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-05-28 20:26:09 +0800

    add most_common_emojis function

--- octo_barnacle/train/agg.py
+++ octo_barnacle/train/agg.py
@@ -1,6 +1,8 @@
 """Provide functions for analysis stored stikers in mongo
 
 """
+import functools
+import heapq
 
 
 def aggregate_emojis(db):
@@ -32,3 +34,41 @@ def count_emojis(db):
         {"$group": {"_id": "$emoji", "total": {"$sum": 1}}},
         {"$sort": {"total": -1}}
     ])
+
+
+def most_common_emojis(db, limit=10):
+    """get top most common emojis in stored stickers
+
+    Arguments:
+        db (pymongo.database.Database): Mongo database that store stickers
+        limit (int): number of return emojis
+
+    Returns:
+        list of emoji, length is equal or less than limit
+    """
+    emojis_count = count_emojis(db)
+
+    @functools.total_ordering
+    class _OrderByCount:
+
+        def __init__(self, emoji_count):
+            self.emoji_count = emoji_count
+
+        def __eq__(self, other):
+            return self.emoji_count['total'] == other.emoji_count['total']
+
+        def __lt__(self, other):
+            return self.emoji_count['total'] > other.emoji_count['total']
+
+    pq = []
+
+    for emoji_count in emojis_count:
+        heapq.heappush(pq, _OrderByCount(emoji_count))
+
+    result = []
+    for _ in range(limit):
+        try:
+            result.append(heapq.heappop(pq).emoji_count['_id'])
+        except IndexError:
+            break
+    return result
