35f63919c28d1f37ca912e357adeeacd957d4731
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-06-15 21:21:29 +0800

    organize components

--- web/mark/stories/index.stories.tsx
+++ web/mark/stories/index.stories.tsx
@@ -2,9 +2,9 @@ import { h } from 'preact';
 
 import { storiesOf } from '@storybook/preact';
 
-import MarkStickersetForm from '../src/MarkStickersetForm';
-import MarkButton from '../src/MarkButton';
-import NextBatchButton from '../src/NextBatchButton';
+import MarkStickersetForm from '../src/components/MarkStickersetForm';
+import MarkButton from '../src/components/MarkButton';
+import NextBatchButton from '../src/components/NextBatchButton';
 
 
 storiesOf('MarkStickersetForm', module)
