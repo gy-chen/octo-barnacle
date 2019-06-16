7eaf142a4a7806c5839e442ee4fb2277222ce4d0
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-06-16 17:26:42 +0800

    update storybook webpack config

--- web/mark/.storybook/webpack.config.js
+++ web/mark/.storybook/webpack.config.js
@@ -1,14 +1,31 @@
+const path = require('path');
+const addCssTypes = require('../config/addCssTypes');
 
+module.exports = async ({ config }) => {
+    await addCssTypes(path.join(__dirname, '../src'))
 
-module.exports = ({ config }) => {
-    config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        use: [
-            {
-                loader: 'ts-loader',
-            },
-        ],
-    });
+    config.module.rules = [
+        {
+            test: /\.(ts|tsx)$/,
+            use: [
+                {
+                    loader: 'ts-loader',
+                },
+            ],
+        },
+        {
+            test: /\.css?$/,
+            use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true
+                    }
+                }
+            ]
+        }
+    ];
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
 };
