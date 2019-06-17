79ed42c2e6840ec0e5c2f83eb3bbf246ab0ee3a6
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-06-17 20:40:41 +0800

    implement MarkButton component

--- web/mark/src/components/MarkButton/index.tsx
+++ web/mark/src/components/MarkButton/index.tsx
@@ -1,8 +1,18 @@
 import { h } from 'preact';
+import * as styles from './markButton.css';
 
-
-const MarkButton = () => {
-    return <div>MarkButton</div>;
+interface Props {
+    onClick?: () => void,
+    disabled?: boolean
 }
+
+const MarkButton = (props: Props) => {
+    const { disabled = false, onClick } = props;
+    return (
+        <button className={styles.markButton} disabled={disabled} onClick={onClick}>
+            <div className={styles.markIcon}></div>
+        </button>
+    );
+};
 
 export default MarkButton;
