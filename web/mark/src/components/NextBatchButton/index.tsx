86bdcf5c25b693410d68c94de30c130f1351bf5e
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-06-17 20:48:15 +0800

    implement NextBatchButton component

--- web/mark/src/components/NextBatchButton/index.tsx
+++ web/mark/src/components/NextBatchButton/index.tsx
@@ -1,8 +1,19 @@
 import { h } from 'preact';
+import * as styles from './nextBatchButton.css';
 
+interface Props {
+    onClick?: () => void;
+    disabled?: boolean;
+}
 
-const NextBatchButton = () => {
-    return <div>NextBatchButton</div>;
+const NextBatchButton = (props: Props) => {
+    const { disabled = false, onClick } = props;
+
+    return (
+        <button className={styles.nextBatchButton} disabled={disabled} onClick={onClick}>
+            <div className={styles.nextBatchIcon}></div>
+        </button>
+    );
 }
 
 export default NextBatchButton;
