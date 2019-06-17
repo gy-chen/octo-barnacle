c7d230040bc5dbc00816cd39d22c62196f9dfab2
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-06-17 20:41:14 +0800

    update story of MarkButton component

--- web/mark/stories/index.stories.tsx
+++ web/mark/stories/index.stories.tsx
@@ -1,6 +1,7 @@
 import { h } from 'preact';
 
 import { storiesOf } from '@storybook/preact';
+import { action } from '@storybook/addon-actions';
 
 import MarkStickersetForm from '../src/components/MarkStickersetForm';
 import MarkButton from '../src/components/MarkButton';
@@ -11,7 +12,9 @@ storiesOf('MarkStickersetForm', module)
   .add('basic', () => <MarkStickersetForm />);
 
 storiesOf('MarkButton', module)
-  .add('basic', () => <MarkButton />);
+  .add('basic', () => <MarkButton />)
+  .add('disabled', () => <MarkButton disabled />)
+  .add('onClick', () => <MarkButton onClick={action('onClick')} />);
 
 storiesOf('NextBatchButton', module)
   .add('basic', () => <NextBatchButton />);
